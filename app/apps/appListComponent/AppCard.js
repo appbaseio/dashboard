9b38c887b7ec21589e5041f7d97ad33613603ada
Deepak Grover <deepakgrover333@gmail.com>
2017-03-01 21:03:11 +0530

    update dashboard main-screen design

--- app/apps/appListComponent/AppCard.js
+++ app/apps/appListComponent/AppCard.js
@@ -6,18 +6,6 @@ export default class AppCard extends Component {
 		super(props);
 	}
 
-	componentDidMount() {
-		setTimeout(this.setHeight, 1000);
-	}
-
-	setHeight() {
-		this.maxHeight = 0;
-		$('.appcard').each((index, item) => {
-			this.maxHeight = this.maxHeight > $(item).height() ? this.maxHeight : $(item).height();
-		});
-		$('.appcard').css({height: this.maxHeight});
-	}
-
 	render() {
 		return (
 			<div className="col-xs-12 col-sm-6 col-md-4 appcard">
