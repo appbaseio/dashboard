c5739d7495cb08cd29a13266c764bb823634e709
farhan687 <farhan687@gmail.com>
2017-03-08 10:40:17 +0530

    Show delete button to owner only in list

--- app/views/list/components/ActionButtons.js
+++ app/views/list/components/ActionButtons.js
@@ -14,6 +14,10 @@ export default class ActionButtons extends Component {
 		event.stopPropagation();
 	}
 
+	isDifferentOwner() {
+		return this.props.app && this.props.app.appInfo && appbaseService && appbaseService.userInfo && appbaseService.userInfo.body && this.props.app.appInfo.owner !== appbaseService.userInfo.body.email ? true : false;
+	}
+
 	render() {
 		return (
 			<aside className="options" onClick={this.stopBubble}>
@@ -23,9 +27,13 @@ export default class ActionButtons extends Component {
 				<div className="options-item">
 					<Credentials data-tip="Copy write Credentials" {...this.props} type="writePermission" label="Write" icon="fa-pencil" />
 				</div>
-				<div className="options-item bottom">
-					<DeleteApp {...this.props} />
-				</div>
+				{
+					!this.isDifferentOwner() ? (
+						<div className="options-item bottom">
+							<DeleteApp {...this.props} />
+						</div>
+					) : null
+				}
 			</aside>
 		);
 	}
