a9fc3f792a448628efd7784d47254a9f9e5a6868
farhan687 <farhan687@gmail.com>
2017-03-10 09:40:55 +0530

    Include production env in production webpack file

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -37,6 +37,11 @@ module.exports = {
 		]
 	},
 	plugins: [
+		new webpack.DefinePlugin({
+			"process.env": {
+				NODE_ENV: JSON.stringify("production")
+			}
+		}),
 		new webpack.optimize.DedupePlugin(),
 		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.optimize.UglifyJsPlugin({
