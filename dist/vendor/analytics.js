7d16c98e426bbd3b66a5c65fa17e2632974a56cd
farhan687 <farhan687@gmail.com>
2017-03-10 19:46:25 +0530

    Build

--- dist/vendor/analytics.js
+++ dist/vendor/analytics.js
@@ -2,4 +2,4 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N2HFT7');
+})(window,document,'script','dataLayer','UA-54082612-12');
