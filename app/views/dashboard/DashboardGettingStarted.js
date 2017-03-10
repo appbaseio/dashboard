e1e9f45e96590d242b27f04ba88dad0e1eedac9b
farhan687 <farhan687@gmail.com>
2017-03-10 09:13:21 +0530

    Add intro section for AppDashboard if apiCalls is 0

---
+++ app/views/dashboard/DashboardGettingStarted.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const DashboardGettingStarted = (props) => {
+	return (
+		<div className="ad-dashboard-getting-started-container row">
+			<div className="col-xs-12 col-sm-6">
+				<section className="ad-detail-page-body-card getting-started col-xs-12 p-0">
+					<header className="ad-detail-page-body-card-title col-xs-12">
+						Getting Started
+					</header>
+					<main className="ad-detail-page-body-card-body col-xs-12">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+							Quos ipsam beatae odit veniam unde dolorem vitae debitis quasi, 
+							natus similique alias ducimus id ipsa labore provident inventore saepe eveniet neque.
+						</p>
+					</main>
+				</section>
+			</div>
+			<div className="col-xs-12 col-sm-6">
+				<section className="ad-detail-page-body-card getting-started col-xs-12 p-0">
+					<header className="ad-detail-page-body-card-title col-xs-12">
+						Tutorial
+					</header>
+					<main className="ad-detail-page-body-card-body col-xs-12">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+							Quos ipsam beatae odit veniam unde dolorem vitae debitis quasi, 
+							natus similique alias ducimus id ipsa labore provident inventore saepe eveniet neque.
+						</p>
+					</main>
+				</section>
+			</div>
+		</div>
+	);
+}
+export default DashboardGettingStarted;
