33e3f718d3c27cfb52c2384becf3ad599e8fbd54
farhan687 <farhan687@gmail.com>
2017-03-20 19:45:47 +0530

    Set appbase logo

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ import * as QuickLinks from './shared/QuickLinks';
 export const contextConfig = {
 	"appbase": {
 		"name": "appbase",
-		"logo": "../../../assets/images/appbase/logo.svg",
+		"logo": "../../../assets/images/appbase/logo.png",
 		"logoText": null,
 		"cardIcon": "fa-database",
 		"appDashboard": ["dashboard", "browser", "gem", "mirage", "credentials", "team"],
