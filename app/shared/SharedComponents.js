4c1730c29b3b2dc6a974ce489dcb3bb130b6c03c
farhan687 <farhan687@gmail.com>
2017-04-26 19:01:59 +0530

    Set old dashboard alert

--- app/shared/SharedComponents.js
+++ app/shared/SharedComponents.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactTooltip from 'react-tooltip';
 import {Loader} from 'react-loaders';
 import { appbaseService } from '../service/AppbaseService';
+import { getConfig } from '../config';
 
 export const AppOwner =(props) => {
 	const isDifferentOwner = () => {
@@ -27,4 +28,32 @@ export const Loading =(props) => {
 			<Loader type="ball-clip-rotate-multiple" active={true} />
 		</span>
 	);
+}
+
+export class OldDashboard extends Component {
+	constructor(props) {
+		super(props);
+		const olddashboard = sessionStorage.getItem("olddashboard");
+		this.state = {
+			close: olddashboard === "close" ? true : false
+		};
+		this.onClose = this.onClose.bind(this);
+	}
+	onClose() {
+		this.setState({
+			close: true
+		});
+		sessionStorage.setItem("olddashboard", "close");
+	}
+	markup() {
+		return (
+			<span className="ad-olddashboard">
+				You can switch back to the old dashboard by <a href="https://appbase.io/scalr/apps">clicking here.</a>
+				<button onClick={this.onClose} type="button" className="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			</span>
+		);
+	}
+	render() {
+		return getConfig().name === "appbase" && !this.state.close ? this.markup() : null;
+	}
 }
