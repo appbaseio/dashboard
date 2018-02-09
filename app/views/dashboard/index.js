3d12dcff3de8ed64eec971f73b25cb03997ea4e1
Divyanshu Maithani <div.blackcat@gmail.com>
2018-02-10 00:13:40 +0530

    Update app dashboard header

--- app/views/dashboard/index.js
+++ app/views/dashboard/index.js
@@ -161,9 +161,12 @@ export default class Dashboard extends Component {
 				}}
 			>
 				<div className="ad-detail-page ad-dashboard row">
-					<header className="ad-detail-page-header header-inline-summary header-align-end col-xs-12">
-						<h2 className="ad-detail-page-title">Dashboard</h2>
-						<p>View app usage stats.</p>
+					<header className="ad-detail-page-header header-inline-summary col-xs-12">
+						<div className="flex flex-column">
+							<h2 className="ad-detail-page-title">Dashboard</h2>
+							<p>View app usage stats.</p>
+						</div>
+						<Upgrade plan={this.state.plan} appCount={this.appCount()} />
 					</header>
 					<main className='ad-detail-page-body col-xs-12'>
 						<section className="col-xs-12 col-sm-8">
@@ -182,7 +185,6 @@ export default class Dashboard extends Component {
 						</section>
 						<section className="col-xs-12 col-sm-4">
 							<ApiCallsView plan={this.state.plan} appCount={this.appCount()} />
-							<Upgrade plan={this.state.plan} appCount={this.appCount()} />
 						</section>
 					</main>
 				</div>
