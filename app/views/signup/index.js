547229a5b48086b22bf4b0f6216e508ccd419b86
Siddharth Kothari <sids.aquarius@gmail.com>
2018-05-25 20:49:49 +0530

    separate login and sign up content

--- app/views/signup/index.js
+++ app/views/signup/index.js
@@ -14,31 +14,33 @@ export default class Login extends Component {
 	render() {
 		const { open, close, showModal, login } = this.props;
 		return (
-			<section id="login" className="text-center container">
+			<section id="signup" className="text-center container">
 				<h2>
 					{this.config.login.description}
 				</h2>
 				<p>
-					Sign in to access your appbase.io apps.<br/>
+					Sign up to create an appbase.io app.<br/>
+					- [ ] By creating an account, you agree to our <a href="https://appbase.io/tos">Terms of Service</a> and <a href="https://appbase.io/privacy">Privacy Policy</a>.<br/>
+					- [ ] Yes, I would like to receive a monthly e-mail on Appbase products, use-cases and promotions via e-mail.
 				</p>
 				<div className="flex flex-column" id="login-btn-screen">
 					<button
 						className="btn Login-button modal-btn github-btn"
 						onClick={() => login('github')}
 					>
-						<i className="fab fa-github" />Sign in with GitHub
+						<i className="fab fa-github" />Sign up with GitHub
 					</button>
 					<button
 						className="btn Login-button modal-btn google-btn"
 						onClick={() => login('google')}
 					>
-						<i className="fab fa-google-plus-g" />Sign in with Google
+						<i className="fab fa-google-plus-g" />Sign up with Google
 					</button>
 				</div>
 				<div className="mt25">
 					<p className="no-margin">
-						Don't have an account?&nbsp;
-						<a href="/signup">Sign up here</a>.
+						Having issues signing up? Write to us&nbsp;
+						<a className="contact-link" href="mailto:info@appbase.io?subject=Login+issues" target="_blank">here</a>.
 					</p>
 				</div>
 			</section>
