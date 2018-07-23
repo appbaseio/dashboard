a5ffeabf3537cf8c2c981b5df9a4e8f575ce8ac7
kuldeep <kuldeep@Siddharths-Air.Dlink>
2018-07-23 20:26:44 +0530

    feat(): Analytics Init

---
+++ src/views/analytics/components/Searches.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Card, Table, Button } from 'antd';
+
+const dataSource = [
+	{
+		key: '1',
+		term: 'NPM',
+		count: 32,
+	},
+	{
+		key: '2',
+		term: '2-228',
+		count: 42,
+	},
+	{
+		key: '3',
+		term: 'PM',
+		count: 42,
+	},
+];
+
+const columns = [
+	{
+		title: 'Search Terms',
+		dataIndex: 'term',
+		key: 'term',
+	},
+	{
+		title: 'Total Queries',
+		dataIndex: 'count',
+		key: 'count',
+	},
+];
+
+const Searches = ({ title }) => (
+	<Card title={title}>
+		<Table dataSource={dataSource} columns={columns} pagination={false} />
+		<Button css="width: 100%;height: 50px;margin-top: 10px;">VIEW ALL</Button>
+	</Card>
+);
+
+export default Searches;
--- src/views/analytics/index.js
+++ src/views/analytics/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Card, Tabs } from 'antd';
 import AppPage from '../../shared/AppPage';
 import { appbaseService } from '../../service/AppbaseService';
+import Flex from './../../shared/Flex';
+import Searches from './components/Searches';
+
+const { TabPane } = Tabs;
 
 export default (props) => {
 	const appName = props.params.appId;
@@ -13,8 +18,26 @@ export default (props) => {
 
 	return (
 		<AppPage pageInfo={pageInfo} key={appId}>
-			<div className="ad-detail-page ad-dashboard row" style={{ padding: '40px 0' }}>
-				<h2>Welcome to analytics</h2>
+			<div className="ad-detail-page ad-dashboard row" style={{ padding: '40px' }}>
+				<Tabs defaultActiveKey="1">
+					<TabPane tab="Analytics" key="1">
+						<Card title="Daily Search Volume" />
+						<Flex css="width: 100%;margin-top: 20px">
+							<div css="flex: 50%;margin-right: 10px">
+								<Searches title="Popular Searches" />
+							</div>
+							<div css="flex: 50%;margin-left: 10px">
+								<Searches title="No Result Searches" />
+							</div>
+						</Flex>
+					</TabPane>
+					<TabPane tab="Popular Searches" key="2">
+						Content of Tab Pane 2
+					</TabPane>
+					<TabPane tab="No Result Searches" key="3">
+						Content of Tab Pane 3
+					</TabPane>
+				</Tabs>
 			</div>
 		</AppPage>
 	);
