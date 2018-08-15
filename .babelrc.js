dc166ad12324483f7f8274c1fbb4c0565ff5a883
Deepak Grover <deepakgrover333@gmail.com>
2018-08-15 14:44:06 +0530

    init setup

---
+++ .babelrc.js
@@ -0,0 +1,22 @@
+const presets = [
+	'@babel/preset-react',
+	[
+		'@babel/env',
+		{
+			targets: {
+				edge: '17',
+				firefox: '60',
+				chrome: '67',
+				safari: '11.1',
+			},
+			useBuiltIns: 'usage',
+		},
+	],
+];
+
+const plugins = [
+	['emotion', { hoist: true }],
+	['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
+];
+
+module.exports = { presets, plugins };
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+	entry: path.join(__dirname, 'src/index.js'),
+	output: {
+		path: path.join(__dirname, 'dist'),
+		publicPath: '/dist/',
+		filename: 'bundle.js',
+		chunkFilename: '[name].bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.less$/,
+				loader: 'less-loader',
+			},
+		],
+	},
+};
