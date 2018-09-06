c46bf725ca1205c4270ba9aed77872d3996c0db3
Deepak Grover <deepakgrover333@gmail.com>
2018-09-06 20:43:02 +0530

    update mappings page + app state flow + batteries

--- src/pages/BrowserPage/BrowserPage.js
+++ src/pages/BrowserPage/BrowserPage.js
@@ -1,53 +1,59 @@
-import React, { Component } from 'react';
-import { Row, Col, Button, Icon } from 'antd';
+import React, { Fragment } from 'react';
+import {
+ Row, Col, Button, Icon,
+} from 'antd';
 
 import Header from '../../components/Header';
 
-export default class BrowserPage extends Component {
-	render() {
-		return (
-			<Header compact>
-				<Row type="flex" justify="space-between" gutter={16}>
-					<Col md={18}>
-						<h2>Browse Data</h2>
+const BrowsePage = ({ appname, appId }) => (
+	<Fragment>
+		<Header compact>
+			<Row type="flex" justify="space-between" gutter={16}>
+				<Col md={18}>
+					<h2>Browse Data</h2>
 
-						<Row>
-							<Col span={18}>
-								<p>
-									Our analytics feature can do much more! Discover what you could
-									do by enabling our metrics on Clicks and Conversions, Filters,
-									Results.
-								</p>
-							</Col>
-						</Row>
-					</Col>
-					<Col
-						md={6}
+					<Row>
+						<Col span={18}>
+							<p>
+								Our analytics feature can do much more! Discover what you could do
+								by enabling our metrics on Clicks and Conversions, Filters, Results.
+							</p>
+						</Col>
+					</Row>
+				</Col>
+				<Col
+					md={6}
+					css={{
+						display: 'flex',
+						flexDirection: 'column-reverse',
+						paddingBottom: 20,
+					}}
+				>
+					<Button size="large" type="primary">
+						<Icon type="customer-service" />
+						Talk to Support
+					</Button>
+					<p
 						css={{
-							display: 'flex',
-							flexDirection: 'column-reverse',
-							paddingBottom: 20,
+							marginTop: 20,
+							fontSize: 13,
+							textAlign: 'center',
+							lineHeight: '20px',
 						}}
 					>
-						<Button size="large" type="primary">
-							<Icon type="customer-service" />
-							Talk to Support
-						</Button>
-						<p
-							css={{
-								marginTop: 20,
-								fontSize: 13,
-								textAlign: 'center',
-								lineHeight: '20px',
-							}}
-						>
-							Need help with your dataset?
-							<br />
-							We now offer paid support.
-						</p>
-					</Col>
-				</Row>
-			</Header>
-		);
-	}
-}
+						Need help with your dataset?
+						<br />
+						We now offer paid support.
+					</p>
+				</Col>
+			</Row>
+		</Header>
+	</Fragment>
+);
+
+BrowsePage.propTypes = {
+	appname: string.isRequired,
+	appId: string.isRequired,
+};
+
+export default BrowsePage;
