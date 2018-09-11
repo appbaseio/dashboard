f65c9a894f2b61dbb704250806af7a3e678621b2
Deepak Grover <deepakgrover333@gmail.com>
2018-09-11 16:54:13 +0530

    transform dashboard actions to use action-factory-method

--- src/reducers/appsReducer.js
+++ src/reducers/appsReducer.js
@@ -4,7 +4,7 @@ import { APPS } from '../constants';
 export default function appsReducer(state: Object = {}, action: Object): ?Object {
 	switch (action.type) {
 		case APPS.LOAD: {
-			return action.apps;
+			return action.payload;
 		}
 		default:
 			return state;
