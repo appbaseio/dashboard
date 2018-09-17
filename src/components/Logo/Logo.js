c60dcda6c2d2265420477d33e6a077367a5f5885
Deepak Grover <deepakgrover333@gmail.com>
2018-09-17 18:09:33 +0530

    Add logo in sidebar
    commit 58fbbf064a454f61b2f0e08f337145fe194a5b6e
    Author: jyash97 <jyash97@gmail.com>
    Date:   Mon Sep 17 18:00:49 2018 +0530
    
        Add logo in sidebar

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { number } from 'prop-types';
 
-const Logo = ({ width }) => <img src="/static/images/appbase.svg" width={width} alt="appbase.io" />;
+const Logo = ({ type, width }) => {
+	switch (type) {
+		case 'white':
+			return <img src="/static/images/appbase_white.svg" width={width} alt="appbase.io" />;
+		case 'small':
+			return <img src="/static/images/appbase_small.svg" width={width} alt="appbase.io" />;
+		default:
+			return <img src="/static/images/appbase.svg" width={width} alt="appbase.io" />;
+	}
+};
 
 Logo.defaultProps = {
 	width: 140,
