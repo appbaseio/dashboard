8e096a5f4dd44f7784c84ffdee318a124c98b5a2
jyash97 <jyash97@gmail.com>
2018-09-17 16:44:03 +0530

    Remove Back component

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
+import { Link } from 'react-router-dom';
 import { string, object } from 'prop-types';
 import { css } from 'react-emotion';
 import { connect } from 'react-redux';
 import get from 'lodash/get';
 
 import UserMenu from './UserMenu';
-import Back from './Back';
 import headerStyles from './styles';
 
 const { Header } = Layout;
@@ -26,7 +26,11 @@ const noBorder = css`
 const AppHeader = ({ currentApp, user }) => (
 	<Header className={headerStyles}>
 		<Menu mode="horizontal">
-			<Back url="/" />
+			<Menu.Item key="back" className={noBorder}>
+				<Link to="/">
+					<Icon type="arrow-left" />
+				</Link>
+			</Menu.Item>
 			<Menu.Item key="1" className={noBorder}>
 				<span>{currentApp || 'Loading...'}</span>
 			</Menu.Item>
