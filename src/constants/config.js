08e290b624d7ea7eafc845a040f4a5e3a36d4338
Siddharth Kothari <sids.aquarius@gmail.com>
2018-09-22 13:48:36 +0530

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,7 +4,7 @@ export const BILLING_API = 'https://transactions.appbase.io';
 export const ACC_API = 'https://accapi-staging.bottleneck.io';
 export const SCALR_API = 'https://api-staging.bottleneck.io';
 export const SCALR_URL = 'scalr.api.appbase.io';
-export const IMPORTER_LINK = 'https://opensource.appbase.io/dejavu/importer?app=';
+export const IMPORTER_LINK = 'https://importer.appbase.io/?app=';
 
 export const exampleConfig = [
 	{
