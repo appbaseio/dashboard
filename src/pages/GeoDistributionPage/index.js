c1c236043a7407f3fc1ebcc7dabc0156bbbf95c0
kuldeep <kuldepsaxena155@gmail.com>
2018-09-25 20:49:32 +0530

    fix(Analytics): Misc Issues

--- src/pages/GeoDistributionPage/index.js
+++ src/pages/GeoDistributionPage/index.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import get from 'lodash/get';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Container from '../../components/Container';
-import GeoDistributionPage from '../../batteries/components/analytics/components/GeoDistribution';
+import Banner from '../../batteries/components/shared/UpgradePlan/Banner';
 
-export default () => (
-	<Container>
-		<GeoDistributionPage />
-	</Container>
+import GeoDistributionPage from '../../batteries/components/analytics/components/GeoDistribution';
+import { getAppPlanByName } from '../../batteries/modules/selectors';
+
+const bannerMessagesAnalytics = {
+	free: {
+		title: 'Unlock the ROI impact of your search',
+		description:
+			'Get a paid plan to see actionable analytics on search volume, popular searches, no results, track clicks and conversions.',
+		buttonText: 'Upgrade Now',
+		href: 'billing',
+	},
+	bootstrap: {
+		title: 'Get richer analytics on clicks and conversions',
+		description:
+			'By upgrading to the Growth plan, you can get more actionable analytics on popular filters, popular results, and track clicks and conversions along with a 30-day retention.',
+		buttonText: 'Upgrade To Growth',
+		href: 'billing',
+	},
+	growth: {
+		title: 'Learn how to track click analytics',
+		description:
+			'See our docs on how to track search, filters, click events, conversions and your own custom events.',
+		buttonText: 'Read Docs',
+		href: 'https://docs.appbase.io',
+	},
+};
+
+const PopularResultsWrapper = ({ plan, isGrowth }) => (
+	<React.Fragment>
+		{isGrowth ? (
+			<React.Fragment>
+				{bannerMessagesAnalytics[plan] && <Banner {...bannerMessagesAnalytics[plan]} />}
+				<Container>
+					<GeoDistributionPage />
+				</Container>
+			</React.Fragment>
+		) : (
+			<Banner {...bannerMessagesAnalytics[plan]} />
+		)}
+	</React.Fragment>
 );
+
+PopularResultsWrapper.propTypes = {
+	plan: PropTypes.string.isRequired,
+	isGrowth: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => {
+	const appPlan = getAppPlanByName(state);
+	return {
+		plan: get(appPlan, 'plan', 'free'),
+		isGrowth: get(appPlan, 'isGrowth'),
+	};
+};
+export default connect(mapStateToProps)(PopularResultsWrapper);
