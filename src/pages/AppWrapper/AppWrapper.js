bbb4bf78cffd1c2ae73ccc51d515951ce8bdca1c
kuldeep <kuldepsaxena155@gmail.com>
2018-09-28 22:44:09 +0530

    fix(): Minor Fixes

--- src/pages/AppWrapper/AppWrapper.js
+++ src/pages/AppWrapper/AppWrapper.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import { Link, Switch, Route } from 'react-router-dom';
-import Loadable from 'react-loadable';
+import { Link } from 'react-router-dom';
 import { Layout, Menu, Icon } from 'antd';
 import { connect } from 'react-redux';
 import get from 'lodash/get';
-
-import AppPageContainer from '../../components/AppPageContainer';
-import AppHeader from '../../components/AppHeader';
-import ShareSettings from '../ShareSettingsPage';
-import Loader from '../../components/Loader';
+import AppLayout from '../../components/AppLayout';
 import { setCurrentApp } from '../../batteries/modules/actions';
 import UpgradeButton from '../../components/Button/UpgradeBtnSidebar';
 import Logo from '../../components/Logo';
@@ -18,79 +13,6 @@ import { breakpoints } from '../../utils/media';
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
-
-const AnalyticsPage = Loadable({
-	loader: () => import('../AnalyticsPage'),
-	loading: Loader,
-});
-const GeoDistributionPage = Loadable({
-	loader: () => import('../GeoDistributionPage'),
-	loading: Loader,
-});
-const SearchLatency = Loadable({
-	loader: () => import('../SearchLatency'),
-	loading: Loader,
-});
-
-const BillingPage = Loadable({
-	loader: () => import('../BillingPage'),
-	loading: Loader,
-});
-
-const CredentialsPage = Loadable({
-	loader: () => import('../CredentialsPage'),
-	loading: Loader,
-});
-
-const OverviewPage = Loadable({
-	loader: () => import('../OverviewPage'),
-	loading: Loader,
-});
-
-const ImporterPage = Loadable({
-	loader: () => import('../ImporterPage'),
-	loading: Loader,
-});
-
-const MappingsPage = Loadable({
-	loader: () => import('../MappingsPage'),
-	loading: Loader,
-});
-
-const BrowserPage = Loadable({
-	loader: () => import('../BrowserPage'),
-	loading: Loader,
-});
-
-const SandboxPage = Loadable({
-	loader: () => import('../SandboxPage'),
-	loading: Loader,
-});
-
-const PopularSearches = Loadable({
-	loader: () => import('../PopularSearches'),
-	loading: Loader,
-});
-
-const PopularResults = Loadable({
-	loader: () => import('../PopularResults'),
-	loading: Loader,
-});
-
-const PopularFilters = Loadable({
-	loader: () => import('../PopularFilters'),
-	loading: Loader,
-});
-
-const NoResultSearches = Loadable({
-	loader: () => import('../NoResultSearches'),
-	loading: Loader,
-});
-
-const RequestLogs = Loadable({
-	loader: () => import('../RequestLogs'),
-	loading: Loader,
-});
 
 const routes = {
 	'App Overview': {
@@ -132,29 +54,76 @@ const routes = {
 	},
 };
 
+const getActiveMenu = (props, prevActiveSubMenu = []) => {
+	let activeSubMenu = 'App Overview';
+	let activeMenuItem = 'App Overview';
+	let { route: pathname } = props.match.params; // eslint-disable-line
+
+	if (!pathname) {
+		pathname = getParam('view') || '';
+	}
+
+	Object.keys(routes).some((route) => {
+		if (routes[route].menu) {
+			const active = routes[route].menu.find(item => pathname === item.link);
+
+			if (active) {
+				activeSubMenu = route;
+				activeMenuItem = active.label;
+				return true;
+			}
+		} else if (pathname === routes[route].link) {
+			activeSubMenu = route;
+			activeMenuItem = route;
+			return true;
+		}
+		return false;
+	});
+	if (prevActiveSubMenu.includes(activeSubMenu)) {
+		return {
+			activeSubMenu: prevActiveSubMenu,
+			activeMenuItem: [activeMenuItem],
+		};
+	}
+	return {
+		activeSubMenu: [activeSubMenu, ...prevActiveSubMenu],
+		activeMenuItem: [activeMenuItem],
+	};
+};
+
+let url;
 class AppWrapper extends Component {
 	constructor(props) {
 		super(props);
 		const collapsed = window.innerWidth <= breakpoints.medium;
+		const getActiveMenuData = getActiveMenu(props);
 		this.state = {
 			collapsed,
 			appName: props.match.params.appName, // eslint-disable-line
+			...getActiveMenuData,
 		};
 	}
 
 	static getDerivedStateFromProps(props, state) {
 		const { appName } = props.match.params;
 		const { currentApp } = props;
-
+		let setActiveMenu = null;
+		if (props.match.url !== url) {
+			setActiveMenu = {
+				...getActiveMenu(props, state.activeSubMenu),
+				url: props.match.url,
+			};
+		}
+		({ url } = props.match);
 		if (appName && appName !== state.appName) {
-			return { appName };
+			return { appName, ...setActiveMenu };
 		}
 
 		if (!appName && !state.appName && currentApp) {
 			// gets last used appName from redux-persist
-			return { appName: currentApp };
+			return { appName: currentApp, ...setActiveMenu };
 		}
-		return null;
+		return { ...setActiveMenu };
 	}
 
 	componentDidMount() {
@@ -182,42 +151,10 @@ class AppWrapper extends Component {
 		this.setState({ collapsed });
 	};
 
-	getActiveMenu = () => {
-		let activeSubMenu = 'App Overview';
-		let activeMenuItem = 'App Overview';
-		let { route: pathname } = this.props.match.params; // eslint-disable-line
-
-		if (!pathname) {
-			pathname = getParam('view') || '';
-		}
-
-		Object.keys(routes).some((route) => {
-			if (routes[route].menu) {
-				const active = routes[route].menu.find(item => pathname === item.link);
-
-				if (active) {
-					activeSubMenu = route;
-					activeMenuItem = active.label;
-					return true;
-				}
-			} else if (pathname === routes[route].link) {
-				activeSubMenu = route;
-				activeMenuItem = route;
-				return true;
-			}
-			return false;
-		});
-
-		return {
-			activeSubMenu,
-			activeMenuItem,
-		};
-	};
-
 	render() {
-		const { activeSubMenu, activeMenuItem } = this.getActiveMenu();
-		const { collapsed, appName } = this.state;
-
+		const {
+ collapsed, appName, activeSubMenu, activeMenuItem,
+} = this.state;
 		return (
 			<Layout>
 				<Sider
@@ -233,14 +170,19 @@ class AppWrapper extends Component {
 				>
 					<Menu
 						theme="dark"
-						defaultOpenKeys={[activeSubMenu]}
-						defaultSelectedKeys={[activeMenuItem]}
+						openKeys={activeSubMenu}
+						selectedKeys={activeMenuItem}
 						mode="inline"
 						css={{
 							overflow: 'auto',
 							position: 'absolute',
 							width: '100%',
 							height: 'calc(100% - 102px)',
+						}}
+						onOpenChange={(param) => {
+							this.setState({
+								activeSubMenu: param,
+							});
 						}}
 					>
 						<Menu.Item style={{ margin: '15px auto' }}>
@@ -284,157 +226,7 @@ class AppWrapper extends Component {
 						{!collapsed && <UpgradeButton link={`/app/${appName}/billing`} />}
 					</Menu>
 				</Sider>
-				<Layout
-					css={{
-						paddingTop: 60,
-						minHeight: '100vh',
-						marginLeft: collapsed ? '80px' : '260px',
-					}}
-				>
-					<AppHeader big={collapsed} />
-					<section>
-						<Switch>
-							<Route
-								exact
-								path="/app/:appName"
-								component={props => (
-									<AppPageContainer {...props} component={OverviewPage} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/overview"
-								component={props => (
-									<AppPageContainer {...props} component={OverviewPage} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/analytics/:tab?/:subTab?"
-								component={props => (
-									<AppPageContainer {...props} component={AnalyticsPage} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/popular-searches"
-								component={props => (
-									<AppPageContainer {...props} component={PopularSearches} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/credentials"
-								component={props => (
-									<AppPageContainer {...props} component={CredentialsPage} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/popular-results"
-								component={props => (
-									<AppPageContainer {...props} component={PopularResults} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/geo-distribution"
-								component={props => (
-									<AppPageContainer {...props} component={GeoDistributionPage} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/search-latency"
-								component={props => (
-									<AppPageContainer {...props} component={SearchLatency} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/popular-filters"
-								component={props => (
-									<AppPageContainer {...props} component={PopularFilters} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/request-logs/:tab?"
-								component={props => (
-									<AppPageContainer {...props} component={RequestLogs} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/no-results-searches"
-								component={props => (
-									<AppPageContainer {...props} component={NoResultSearches} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/import"
-								render={props => (
-									<AppPageContainer
-										{...props}
-										component={ImporterPage}
-										shouldFetchAppInfo={false}
-										shouldFetchAppPlan={false}
-									/>
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/mappings"
-								render={props => (
-									<AppPageContainer
-										{...props}
-										component={MappingsPage}
-										shouldFetchAppInfo={false}
-									/>
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/share-settings"
-								component={props => (
-									<AppPageContainer {...props} component={ShareSettings} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/billing"
-								component={props => (
-									<AppPageContainer {...props} component={BillingPage} />
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/browse"
-								render={props => (
-									<AppPageContainer
-										{...props}
-										component={BrowserPage}
-										shouldFetchAppInfo={false}
-										shouldFetchAppPlan={false}
-									/>
-								)}
-							/>
-							<Route
-								exact
-								path="/app/:appName/search-preview"
-								render={props => (
-									<AppPageContainer
-										{...props}
-										component={SandboxPage}
-										shouldFetchAppInfo={false}
-										shouldFetchAppPlan={false}
-									/>
-								)}
-							/>
-						</Switch>
-					</section>
-				</Layout>
+				<AppLayout collapsed={collapsed} {...this.props} />
 			</Layout>
 		);
 	}
