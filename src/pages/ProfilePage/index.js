24254c9cebec593e4962dbdb1ed0dab586b3d34d
jyash97 <jyash97@gmail.com>
2018-10-09 18:42:17 +0530

    Fix : Liniting issue

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -114,7 +114,7 @@ class ProfilePage extends React.Component {
 	render() {
 		const { submitCountryCode } = this.state;
 		const { isSubmitting, username } = this.props;
-    const firstName = username ? username.split(' ')[0] : 'Bud';
+		const firstName = username ? username.split(' ')[0] : 'Bud';
 		return (
 			<React.Fragment>
 				<FullHeader />
@@ -268,7 +268,7 @@ const mapStateToProps = (state) => {
 		deploymentTimeframe: get(state, 'user.data.deployment-timeframe'),
 		phone: get(phoneInfo, 'length') ? phoneInfo.split('-')[1] : '',
 		company: get(state, 'user.data.company'),
-    username: get(state, 'user.data.name'),
+		username: get(state, 'user.data.name'),
 		countryCode: get(phoneInfo, 'length')
 			? get(countryCodes.find(item => item.dial_code === phoneInfo.split('-')[0]), 'code', '')
 			: '',
