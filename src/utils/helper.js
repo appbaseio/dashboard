f9e02668e62819bd622d9466a98f853c5d80e3c2
Yash Joshi <jyash97@gmail.com>
2018-10-17 17:55:35 +0530

    Add appname validations in Tutorial (#96)

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -98,3 +98,21 @@ export const getAppCount = (appStats, plan) => {
 	};
 	return obj;
 };
+
+export const validateAppName = (name) => {
+	const symbolsToCheck = /[\s#&*'"\\|,<>\/?]/; //eslint-disable-line
+	const nameCharacters = name.split('');
+	const startsWith =		nameCharacters[0] === '+' || nameCharacters[0] === '-' || nameCharacters[0] === '_';
+
+	if (name === '.' || name === '..' || name === '' || symbolsToCheck.test(name) || startsWith) {
+		return false;
+	}
+	return true;
+};
+
+export const validationsList = [
+	'Lowercase only',
+	'Cannot include \\, /, *, ?, ", <, >, |, ` ` (space character), ,, #',
+	'Cannot start with -, _, +',
+	'Cannot be . or ..',
+];
