f8d715ee5db2915c77f616539d7e195137bf1fad
jyash97 <jyash97@gmail.com>
2018-11-12 17:53:13 +0530

    Add docs link on navbar

--- src/components/AppHeader/UserMenu.js
+++ src/components/AppHeader/UserMenu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { object } from 'prop-types';
 import {
- Menu, Avatar, Dropdown, Icon,
+ Menu, Avatar, Dropdown, Icon, Row,
 } from 'antd';
 import { css } from 'react-emotion';
 import { media } from '../../utils/media';
@@ -26,6 +26,17 @@ const menuItem = css`
 	align-items: center;
 `;
 
+const link = css`
+	margin-right: 20px;
+	color: rgba(0, 0, 0, 0.65);
+	i {
+		margin-right: 4px;
+	}
+	${media.small(css`
+		display: none;
+	`)};
+`;
+
 const UserMenu = ({ user }) => {
 	const menu = (
 		<Menu>
@@ -42,15 +53,25 @@ const UserMenu = ({ user }) => {
 	);
 
 	return (
-		<Dropdown overlay={menu} className={userMenu} trigger={['click']}>
-			<span style={{ cursor: 'pointer' }}>
-				<Avatar src={user.picture} />
-				&nbsp;&nbsp;
-				{user ? user.name : 'Loading...'}
-				&nbsp;&nbsp;
-				<Icon type="down" />
-			</span>
-		</Dropdown>
+		<Row justify="space-between" align="middle">
+			<a
+				href="https://docs.appbase.io/javascript/quickstart.html"
+				className={link}
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<Icon type="rocket" /> Docs
+			</a>
+			<Dropdown overlay={menu} className={userMenu} trigger={['click']}>
+				<span style={{ cursor: 'pointer' }}>
+					<Avatar src={user.picture} />
+					&nbsp;&nbsp;
+					{user ? user.name : 'Loading...'}
+					&nbsp;&nbsp;
+					<Icon type="down" />
+				</span>
+			</Dropdown>
+		</Row>
 	);
 };
 
