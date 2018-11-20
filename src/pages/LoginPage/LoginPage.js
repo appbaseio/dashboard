ee6317cf4776366fbaf7973e82e4cab74fddbfcd
jyash97 <jyash97@gmail.com>
2018-11-20 14:39:45 +0530

    Add verificat and fix styling

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {
- Card, Button, Icon, Input,
+ Card, Button, Icon, Input, message,
 } from 'antd';
 import { Redirect, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -9,7 +9,7 @@ import PropTypes from 'prop-types';
 import Logo from '../../components/Logo';
 import { ACC_API } from '../../constants/config';
 import {
- container, card, githubBtn, googleBtn, gitlabBtn,
+ container, card, githubBtn, googleBtn, gitlabBtn, emailBtn, inputStyles,
 } from './styles';
 
 const getLoginURL = provider => `${ACC_API}/login/${provider}?next=${window.location.origin}`;
@@ -23,6 +23,9 @@ class LoginPage extends React.Component {
 		this.setState(({ isEmailLogin }) => ({
 			isEmailLogin: !isEmailLogin,
 			emailInput: '',
+			isLoading: false,
+			showOtp: false,
+			otp: '',
 		}));
 	};
 
@@ -35,9 +38,73 @@ class LoginPage extends React.Component {
 		});
 	};
 
+	toggleLoading = () => {
+		this.setState(({ isLoading }) => ({
+			isLoading: !isLoading,
+		}));
+	};
+
+	handleEmailSubmission = async () => {
+		const { emailInput } = this.state;
+		this.toggleLoading();
+		try {
+			const response = await fetch(`${ACC_API}/user/email`, {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json',
+				},
+				body: JSON.stringify({
+					email: emailInput,
+				}),
+			});
+			const data = await response.json();
+			if (response.status >= 400) {
+				message.error(data.message);
+				this.toggleLoading();
+			} else {
+				this.toggleLoading();
+				message.success(data.message);
+				this.setState({
+					showOtp: true,
+				});
+			}
+		} catch (e) {
+			message.error('Something went Wrong.');
+		}
+	};
+
+	verifyOtp = async () => {
+		const { emailInput, otp } = this.state;
+		this.toggleLoading();
+		try {
+			const response = await fetch(`${ACC_API}/user/verify`, {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json',
+				},
+				body: JSON.stringify({
+					email: emailInput,
+					otp,
+				}),
+			});
+			const data = await response.json();
+			if (response.status >= 400) {
+				message.error(data.message);
+				this.toggleLoading();
+			} else {
+				this.toggleLoading();
+				message.success(data.message);
+			}
+		} catch (e) {
+			message.error('Something went Wrong.');
+		}
+	};
+
 	render() {
 		const { user } = this.props;
-		const { isEmailLogin, emailInput } = this.state;
+		const {
+			isEmailLogin, emailInput, isLoading, showOtp, otp,
+		} = this.state; // prettier-ignore
 		if (user.data) {
 			return <Redirect to="/" />;
 		}
@@ -46,44 +113,84 @@ class LoginPage extends React.Component {
 				<Logo width={200} />
 				<Card className={card} bordered={false}>
 					<h2>Sign in to get started</h2>
-					<Button onClick={this.toggleEmailLogin} icon="mail" size="large" block>
-						{isEmailLogin ? 'Hide Input' : 'Sign in via Email'}
-					</Button>
 					{isEmailLogin ? (
 						<Input
 							placeholder="Enter Email"
 							name="emailInput"
+							disabled={showOtp}
 							onChange={this.handleInput}
+							size="large"
+							className={inputStyles}
 							value={emailInput}
 						/>
 					) : null}
-					<Button
-						href={getLoginURL('github')}
-						icon="github"
-						className={githubBtn}
-						size="large"
-						block
-					>
-						Sign in via GitHub
-					</Button>
-					<Button
-						href={getLoginURL('google')}
-						icon="google"
-						className={googleBtn}
-						size="large"
-						block
-					>
-						Sign in via Google
-					</Button>
-					<Button
-						href={getLoginURL('gitlab')}
-						icon="gitlab"
-						className={gitlabBtn}
-						size="small"
-						block
-					>
-						Sign in via Gitlab
-					</Button>
+					{showOtp ? (
+						<React.Fragment>
+							<Input
+								placeholder="Enter OTP"
+								name="otp"
+								onChange={this.handleInput}
+								value={otp}
+								size="large"
+								className={inputStyles}
+							/>
+							<Button
+								onClick={this.verifyOtp}
+								icon="check"
+								size="large"
+								loading={isLoading}
+								className={emailBtn}
+								block
+							>
+								Verify OTP
+							</Button>
+						</React.Fragment>
+					) : (
+						<Button
+							onClick={
+								isEmailLogin ? this.handleEmailSubmission : this.toggleEmailLogin
+							}
+							icon={isEmailLogin ? '' : 'mail'}
+							size="large"
+							loading={isLoading}
+							className={emailBtn}
+							type="primary"
+							block
+						>
+							{isEmailLogin ? 'Submit' : 'Sign in via Email'}
+						</Button>
+					)}
+					{isEmailLogin || (
+						<React.Fragment>
+							<Button
+								href={getLoginURL('github')}
+								icon="github"
+								className={githubBtn}
+								size="large"
+								block
+							>
+								Sign in via GitHub
+							</Button>
+							<Button
+								href={getLoginURL('google')}
+								icon="google"
+								className={googleBtn}
+								size="large"
+								block
+							>
+								Sign in via Google
+							</Button>
+							<Button
+								href={getLoginURL('gitlab')}
+								icon="gitlab"
+								className={gitlabBtn}
+								size="small"
+								block
+							>
+								Sign in via Gitlab
+							</Button>
+						</React.Fragment>
+					)}
 				</Card>
 
 				<Link to="/signup">
