91a8e1da5619f7a050d3259dcbff289dd5ed8327
jyash97 <jyash97@gmail.com>
2018-11-27 16:09:29 +0530

    Optimize mobile UI

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -4,7 +4,9 @@ import React, { Component, Fragment } from 'react';
 import {
  Switch, Route, Redirect, withRouter,
 } from 'react-router-dom';
-import { Menu } from 'antd';
+import {
+ Menu, Dropdown, Button, Icon,
+} from 'antd';
 
 import Account from './Account';
 import Email from './Email';
@@ -36,6 +38,32 @@ class Profile extends Component<Props> {
 			},
 		} = this.props;
 		const pathParts = pathname.split('/');
+		const menu = (
+			<Menu
+				onClick={this.handleMenuClick}
+				defaultSelectedKeys={[pathParts[2] || 'account']}
+				mode={window.innerWidth <= breakpoints.medium ? 'horizontal' : 'inline'}
+				css={{
+					backgroundColor: 'transparent',
+					borderRight: 0,
+					marginRight: 15,
+					width: '180px',
+					[mediaKey.medium]: {
+						marginRight: 0,
+						width: '100% !important',
+					},
+					[mediaKey.small]: {
+						backgroundColor: 'white',
+						width: '100% !important',
+					},
+				}}
+			>
+				<Item key="account">Account</Item>
+				<Item key="email">Email</Item>
+				<Item key="credentials">Credentials</Item>
+				<Item key="close">Close Account</Item>
+			</Menu>
+		);
 
 		return (
 			<Fragment>
@@ -54,27 +82,27 @@ class Profile extends Component<Props> {
 						},
 					}}
 				>
-					<div>
-						<Menu
-							onClick={this.handleMenuClick}
-							defaultSelectedKeys={[pathParts[2] || 'account']}
-							mode={window.innerWidth <= breakpoints.medium ? 'horizontal' : 'inline'}
-							css={{
-								backgroundColor: 'transparent',
-								borderRight: 0,
-								marginRight: 15,
-								width: '180px',
-								[mediaKey.medium]: {
-									marginRight: 0,
-									width: '100% !important',
-								},
-							}}
-						>
-							<Item key="account">Account</Item>
-							<Item key="email">Email</Item>
-							<Item key="credentials">Credentials</Item>
-							<Item key="close">Close Account</Item>
-						</Menu>
+					<div
+						css={{
+							[mediaKey.small]: {
+								padding: 10,
+							},
+						}}
+					>
+						{window.innerWidth <= 425 ? (
+							<Dropdown overlay={menu}>
+								<Button
+									style={{
+										width: '100%',
+										textTransform: 'capitalize',
+									}}
+								>
+									{[pathParts[2] || 'account']} <Icon type="down" />
+								</Button>
+							</Dropdown>
+						) : (
+							menu
+						)}
 					</div>
 					<div
 						css={{
