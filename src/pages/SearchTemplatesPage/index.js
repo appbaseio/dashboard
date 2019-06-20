3d249681438cefe7f869a95391141630b34351dd
kuldeep <kuldepsaxena155@gmail.com>
2019-06-20 15:59:54 +0530

    feat: restrict templates UI only for paid users

--- src/pages/SearchTemplatesPage/index.js
+++ src/pages/SearchTemplatesPage/index.js
@@ -1,317 +1,55 @@
 import React from 'react';
+import get from 'lodash/get';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Table, Card, notification } from 'antd';
-import get from 'lodash/get';
-import { FormBuilder, Validators } from 'react-reactive-form';
 import { css } from 'emotion';
-import { displayErrors } from '../../utils/helper';
-import Loader from '../../batteries/components/shared/Loader/Spinner';
-import Container from '../../components/Container';
+import Overlay from '../../components/Overlay';
 import Banner from '../../batteries/components/shared/UpgradePlan/Banner';
-import {
-	getAppTemplates,
-	saveAppTemplate,
-	deleteAppTemplate,
-	validateAppTemplate,
-} from '../../batteries/modules/actions';
-import { getAppTemplatesByName } from '../../batteries/modules/selectors';
-import Actions from './Actions';
-import CreateTemplate from './CreateTemplate';
-import GetAPIEndpoint from './GetAPIEndpoint';
-import { jsonValidator } from './utils';
+import { getAppPlanByName } from '../../batteries/modules/selectors';
+import SearchTemplates from './SearchTemplates';
 
-const columns = [
-	{
-		title: 'Name',
-		dataIndex: 'template_id',
-		key: 'name',
-	},
-	{
-		title: 'Actions',
-		key: 'actions',
-		width: 400,
-		render: item => (
-			<Actions
-				handleRender={() => item.handleRender(item.template_id)}
-				handleEdit={() => item.handleEdit(item.template_id)}
-				handleDelete={() => item.handleDelete(item.template_id)}
-			/>
-		),
-	},
-];
+const bannerMessagesAnalytics = {
+	title: 'Unlock the ROI impact of your search',
+	description:
+		'Get a paid plan to see actionable analytics on search volume, popular searches, no results, track clicks and conversions.',
+	buttonText: 'Upgrade Now',
+	href: 'billing',
+};
 
-const main = css`
-	.actionBtn {
-		position: absolute;
-		right: 50px;
-	}
-`;
-
-class SearchTemplatesPage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			createMode: false,
-			editMode: false,
-			copyEndpoint: false,
-			currentTemplate: null,
-		};
-		this.getTemplates();
-		this.form = FormBuilder.group({
-			name: ['', [Validators.required, Validators.pattern(/^(\d|\w)+$/)]],
-			query: [null, [Validators.required, jsonValidator]],
-		});
-		this.templateRef = React.createRef();
-		this.bannerProps = {
-			default: {
-				title: 'Search Templates',
-				description: 'GUI to manage your search templates.',
-				buttonText: 'Create Template',
-				icon: 'plus',
-				onClick: () => this.toggleCreateMode(),
-			},
-			create: {
-				title: 'Create Template',
-				buttonText: 'Read More',
-				showGoBack: true,
-				goBackText: 'Go back to templates',
-				onClickGoBack: () => this.toggleCreateMode(),
-				href: 'https://docs.appbase.io/',
-			},
-			edit: {
-				title: 'Edit Template',
-				buttonText: 'Read More',
-				showGoBack: true,
-				goBackText: 'Go back to templates',
-				onClickGoBack: () => this.toggleEditMode(),
-				href: 'https://docs.appbase.io/',
-			},
-		};
-	}
-
-	componentDidUpdate(prevProps) {
-		const { errors } = this.props;
-		displayErrors(errors, prevProps.errors, true);
-	}
-
-	get bannerDetails() {
-		const { createMode, editMode } = this.state;
-		if (editMode) {
-			return this.bannerProps.edit;
-		}
-		if (createMode) {
-			return this.bannerProps.create;
-		}
-		return this.bannerProps.default;
-	}
-
-	getTemplates = () => {
-		const { fetchTemplates } = this.props;
-		fetchTemplates();
-	};
-
-	toggleCreateMode = (status) => {
-		this.form.reset();
-		this.setState(prevState => ({
-			createMode: status === undefined ? !prevState.createMode : status,
-			currentTemplate: null,
-		}));
-	};
-
-	toggleEditMode = (status) => {
-		this.setState(prevState => ({
-			currentTemplate: prevState.editMode ? null : prevState.currentTemplate,
-			editMode: status === undefined ? !prevState.editMode : status,
-		}));
-	};
-
-	togglecopyEndpoint = (status) => {
-		this.setState(prevState => ({
-			currentTemplate: prevState.copyEndpoint ? null : prevState.currentTemplate,
-			copyEndpoint: status === undefined ? !prevState.copyEndpoint : status,
-		}));
-	};
-
-	handleSaveTemplate = () => {
-		try {
-			const { saveTemplate } = this.props;
-			const { name, query } = this.form.getRawValue();
-			const requestBody = JSON.parse(query);
-			if (!this.form.valid) {
-				throw new Error('Please fill the form details first.');
-			}
-			if (!requestBody.source) {
-				throw new Error('Invalid query, `source` key is missing.');
-			}
-			saveTemplate(name, requestBody).then((action) => {
-				if (get(action, 'payload.acknowledged')) {
-					notification.success({
-						message: 'Template saved successfully.',
-					});
-					this.getTemplates();
-					this.toggleEditMode(false);
-					this.toggleCreateMode(false);
-				}
-			});
-		} catch (e) {
-			notification.error({
-				message: e.message,
-			});
-		}
-	};
-
-	handleValidateTemplate = (templateId) => {
-		const { validateTemplate } = this.props;
-		const { query } = this.form.value;
-		const queryControl = this.form.get('query');
-		const requestBody = JSON.parse(query);
-		try {
-			if (!requestBody.source) {
-				throw new Error('Invalid query, `source` key is missing.');
-			}
-			if (!requestBody.params) {
-				throw new Error('Invalid query, `params` key is missing.');
-			}
-			if (!queryControl.valid) {
-				throw new Error('Please enter valid JSON query.');
-			}
-			validateTemplate(requestBody, templateId).then((action) => {
-				if (get(action, 'payload')) {
-					window.scrollTo(0, document.body.scrollHeight);
-				}
-			});
-		} catch (e) {
-			notification.error({
-				message: e.message,
-			});
-		}
-	};
-
-	handleDelete = (id) => {
-		const { deleteTemplate } = this.props;
-		deleteTemplate(id).then((action) => {
-			if (get(action, 'payload.acknowledged')) {
-				notification.success({
-					message: 'Template deleted successfully.',
-				});
-				this.getTemplates();
-			}
-		});
-	};
-
-	handleEdit = (id) => {
-		this.setState(
-			{
-				currentTemplate: id,
-			},
-			this.toggleEditMode,
-		);
-	};
-
-	handleRender = (id) => {
-		this.setState(
-			{
-				currentTemplate: id,
-			},
-			this.togglecopyEndpoint,
-		);
-	};
-
-	render() {
-		const {
- createMode, editMode, currentTemplate, copyEndpoint,
-} = this.state;
-		const { isLoading, templates, isDeleting } = this.props;
-		const isDefault = !(createMode || editMode);
-		if (isLoading && !(templates && templates.length)) {
-			return <Loader />;
-		}
-		return (
+const TemplatesView = ({ isPaidUser }) => (
+	<React.Fragment>
+		{isPaidUser ? (
+			<SearchTemplates />
+		) : (
 			<React.Fragment>
-				<Banner {...this.bannerDetails} />
-				<Container css={main}>
-					{isDefault && (
-						<Card
-							style={
-								isDeleting
-									? {
-											pointerEvents: 'none',
-											opacity: 0.6,
-									  }
-									: null
-							}
-						>
-							<Table
-								rowKey={item => item.template_id}
-								dataSource={templates.map(item => ({
-									handleDelete: this.handleDelete,
-									handleEdit: this.handleEdit,
-									handleRender: this.handleRender,
-									...item,
-								}))}
-								columns={columns}
-							/>
-						</Card>
-					)}
-					{(createMode || editMode) && (
-						<CreateTemplate
-							handleValidateTemplate={() => this.handleValidateTemplate()}
-							handleSaveTemplate={this.handleSaveTemplate}
-							control={this.form}
-							templateId={currentTemplate}
-						/>
-					)}
-					{copyEndpoint && (
-						<GetAPIEndpoint
-							templateId={currentTemplate}
-							visible={copyEndpoint}
-							handleCancel={this.togglecopyEndpoint}
-						/>
-					)}
-				</Container>
+				<Banner {...bannerMessagesAnalytics} />
+				<Overlay
+					style={{
+						maxWidth: '70%',
+					}}
+					lockSectionClassName={css`
+						display: none;
+						.button {
+							display: none;
+						}
+					`}
+					src="/static/images/templates_banner.png"
+					alt="analytics"
+				/>
 			</React.Fragment>
-		);
-	}
-}
-
-SearchTemplatesPage.defaultProps = {
-	templates: [],
+		)}
+	</React.Fragment>
+);
+TemplatesView.propTypes = {
+	isPaidUser: PropTypes.bool.isRequired,
+	plan: PropTypes.string.isRequired,
 };
 
-SearchTemplatesPage.propTypes = {
-	isLoading: PropTypes.bool.isRequired,
-	isDeleting: PropTypes.bool.isRequired,
-	appName: PropTypes.string.isRequired,
-	fetchTemplates: PropTypes.func.isRequired,
-	saveTemplate: PropTypes.func.isRequired,
-	deleteTemplate: PropTypes.func.isRequired,
-	validateTemplate: PropTypes.func.isRequired,
-	templates: PropTypes.array,
-	errors: PropTypes.array.isRequired,
+const mapStateToProps = (state) => {
+	const appPlan = getAppPlanByName(state);
+	return {
+		plan: get(appPlan, 'plan'),
+		isPaidUser: get(appPlan, 'isPaid'),
+	};
 };
-
-const mapStateToProps = state => ({
-	templates: getAppTemplatesByName(state),
-	appName: get(state, '$getCurrentApp.name'),
-	isLoading: get(state, '$getAppTemplates.isFetching', false),
-	isDeleting: get(state, '$deleteAppTemplate.isFetching', false),
-	errors: [
-		get(state, '$getAppTemplates.error'),
-		get(state, '$saveAppTemplate.error'),
-		get(state, '$deleteAppTemplate.error'),
-		get(state, '$validateAppTemplate.error'),
-	],
-});
-
-const mapDispatchToProps = dispatch => ({
-	fetchTemplates: () => dispatch(getAppTemplates()),
-	saveTemplate: (name, payload) => dispatch(saveAppTemplate(name, payload)),
-	deleteTemplate: name => dispatch(deleteAppTemplate(name)),
-	validateTemplate: (payload, name) => dispatch(validateAppTemplate(payload, name)),
-});
-
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps,
-)(SearchTemplatesPage);
+export default connect(mapStateToProps)(TemplatesView);
