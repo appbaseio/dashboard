8d556feae74b57d6bede4d7fb05bc0e875ab9c92
Siddharth Kothari <sids.aquarius@gmail.com>
2019-06-28 16:07:32 +0530

    WIP: test env change

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 
-fs.writeFileSync(
-	'./.env',
-	`SENTRY_AUTH_TOKEN=${process.env.SENTRY_TOKEN}
+const envFile = `SENTRY_AUTH_TOKEN=${process.env.SENTRY_TOKEN}
 SENTRY_URL=https://sentry.io/
 SENTRY_ORG=appbase-1
 SENTRY_PROJECT=frontend
-CONTEXT=${process.env.CONTEXT}`,
-);
+CONTEXT=${process.env.CONTEXT}`;
+
+// eslint-disable-next-line no-console
+console.log('envFile: ', envFile);
+
+// eslint-disable-next-line no-console
+console.log('all envs: ', process.env);
+
+fs.writeFileSync('./.env', envFile);
