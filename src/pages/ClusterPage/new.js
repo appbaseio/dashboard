07cc0a5f1f4eb7a9de036bccd3dd21e8c289910b
Siddharth Kothari <sids.aquarius@gmail.com>
2019-09-30 07:44:54 +0530

    chore: update ES, ODFE, Arc cluster versions (#274)

--- src/pages/ClusterPage/new.js
+++ src/pages/ClusterPage/new.js
@@ -22,11 +22,11 @@ const { TabPane } = Tabs;
 const SSH_KEY =	'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCVqOPpNuX53J+uIpP0KssFRZToMV2Zy/peG3wYHvWZkDvlxLFqGTikH8MQagt01Slmn+mNfHpg6dm5NiKfmMObm5LbcJ62Nk9AtHF3BPP42WyQ3QiGZCjJOX0fVsyv3w3eB+Eq+F+9aH/uajdI+wWRviYB+ljhprZbNZyockc6V33WLeY+EeRQW0Cp9xHGQUKwJa7Ch8/lRkNi9QE6n5W/T6nRuOvu2+ThhjiDFdu2suq3V4GMlEBBS6zByT9Ct5ryJgkVJh6d/pbocVWw99mYyVm9MNp2RD9w8R2qytRO8cWvTO/KvsAZPXj6nJtB9LaUtHDzxe9o4AVXxzeuMTzx siddharth@appbase.io';
 
 const esVersions = [
-	'7.3.0',
+	'7.3.2',
 	'7.2.1',
 	'7.1.1',
 	'7.0.1',
-	'6.8.2',
+	'6.8.3',
 	'6.7.2',
 	'6.6.2',
 	'6.5.4',
@@ -38,9 +38,9 @@ const esVersions = [
 	'5.6.16',
 ];
 
-const odfeVersions = ['1.1.0', '0.9.0'];
+const odfeVersions = ['1.2.0', '1.1.0', '0.9.0'];
 
-const V7_ARC = '7.2.0-appbase';
+const V7_ARC = '7.2.2-appbase';
 const V6_ARC = '0.1.6';
 const V5_ARC = 'v5-0.0.1';
 
