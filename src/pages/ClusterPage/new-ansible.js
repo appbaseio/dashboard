02b12e2d8dc6fefda074494c6299abaa40f53ef9
lakhansamani <lakhan.m.samani@gmail.com>
2020-06-06 07:15:43 +0530

    chore: add 7.7.1 es version in ansible page

--- src/pages/ClusterPage/new-ansible.js
+++ src/pages/ClusterPage/new-ansible.js
@@ -28,6 +28,7 @@ const SSH_KEY =
 	'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCVqOPpNuX53J+uIpP0KssFRZToMV2Zy/peG3wYHvWZkDvlxLFqGTikH8MQagt01Slmn+mNfHpg6dm5NiKfmMObm5LbcJ62Nk9AtHF3BPP42WyQ3QiGZCjJOX0fVsyv3w3eB+Eq+F+9aH/uajdI+wWRviYB+ljhprZbNZyockc6V33WLeY+EeRQW0Cp9xHGQUKwJa7Ch8/lRkNi9QE6n5W/T6nRuOvu2+ThhjiDFdu2suq3V4GMlEBBS6zByT9Ct5ryJgkVJh6d/pbocVWw99mYyVm9MNp2RD9w8R2qytRO8cWvTO/KvsAZPXj6nJtB9LaUtHDzxe9o4AVXxzeuMTzx siddharth@appbase.io';
 
 const esVersions = [
+	'7.7.1',
 	'7.7.0',
 	'7.6.2',
 	'7.5.2',
