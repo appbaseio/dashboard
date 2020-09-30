5da23726f2fb3c3784b25be78e82474dd46756c1
Siddharth Kothari <sids.aquarius@gmail.com>
2020-09-30 21:00:13 +0530

    chore: update arc version

--- src/pages/ClusterPage/new.js
+++ src/pages/ClusterPage/new.js
@@ -35,9 +35,9 @@ const esVersions = ['7.9.0', '7.8.1', '7.8.0', '7.7.1'];
 
 const odfeVersions = ['1.9.0', '1.8.0'];
 
-export const V7_ARC = '7.32.0-cluster';
-export const V6_ARC = '7.32.0-cluster';
-export const ARC_BYOC = '7.32.0-byoc';
+export const V7_ARC = '7.32.1-cluster';
+export const V6_ARC = '7.32.1-cluster';
+export const ARC_BYOC = '7.32.1-byoc';
 export const V5_ARC = 'v5-0.0.1';
 
 export const arcVersions = {
