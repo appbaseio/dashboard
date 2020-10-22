8be6093af3128e8fe23742ce5653b81f26c0bcde
Lakhan Samani <lakhan.m.samani@gmail.com>
2020-10-22 09:37:32 +0530

    chore: update to arc 7.34.0

--- src/pages/ClusterPage/new.js
+++ src/pages/ClusterPage/new.js
@@ -35,9 +35,9 @@ const esVersions = ['7.9.0', '7.8.1', '7.8.0', '7.7.1'];
 
 const odfeVersions = ['1.9.0', '1.8.0'];
 
-export const V7_ARC = '7.33.0-cluster';
-export const V6_ARC = '7.33.0-cluster';
-export const ARC_BYOC = '7.33.0-byoc';
+export const V7_ARC = '7.34.0-cluster';
+export const V6_ARC = '7.34.0-cluster';
+export const ARC_BYOC = '7.34.0-byoc';
 export const V5_ARC = 'v5-0.0.1';
 
 export const arcVersions = {
