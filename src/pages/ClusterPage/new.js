9878ec7ce3171946461209066e5f6a152701790b
Siddharth Kothari <siddharth@appbase.io>
2021-09-22 15:01:25 +0530

    chore: update appbase.io release version (#449)

--- src/pages/ClusterPage/new.js
+++ src/pages/ClusterPage/new.js
@@ -35,9 +35,9 @@ const esVersions = ['7.10.2'];
 
 const openSearchVersions = ['1.0.0'];
 
-export const V7_ARC = '7.49.1-cluster';
-export const V6_ARC = '7.49.1-cluster';
-export const ARC_BYOC = '7.49.1-byoc';
+export const V7_ARC = '7.49.2-cluster';
+export const V6_ARC = '7.49.2-cluster';
+export const ARC_BYOC = '7.49.2-byoc';
 export const V5_ARC = 'v5-0.0.1';
 
 export const arcVersions = {
