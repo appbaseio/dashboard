1b552cf6b6006c8779741864f5257d8316e5912f
lohi212 <suvvarilohita@gmail.com>
2022-03-17 07:36:54 +0530

    feat(login): branding changes

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -5,7 +5,7 @@ import { Redirect, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import EmailAuth from './EmailAuth';
-import Logo from '../../components/Logo';
+import ReactivesearchLogo from '../../components/Logo/ReactivesearchLogo';
 import { ACC_API } from '../../constants/config';
 import { container, card, githubBtn, googleBtn, gitlabBtn } from './styles';
 import { getUrlParams } from '../../utils/helper';
@@ -47,7 +47,7 @@ class LoginPage extends React.Component {
 		}
 		return (
 			<section className={container}>
-				<Logo width={200} />
+				<ReactivesearchLogo width={250} />
 				<Card className={card} bordered={false}>
 					<h2>Sign in to get started</h2>
 					{isEmailLogin || (
@@ -101,7 +101,7 @@ class LoginPage extends React.Component {
 							letterSpacing: '0.02rem',
 						}}
 					>
-						New to appbase.io? &nbsp; Signup here
+						New to reactivesearch.io? &nbsp; Signup here
 						<Icon type="arrow-right" />
 					</Button>
 				</Link>
