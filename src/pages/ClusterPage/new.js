0b01d166115401de6d82893cb8ed29870b387384
Siddharth Kothari <sids.aquarius@gmail.com>
2022-08-29 16:04:49 +0530

    Update new.js

--- src/pages/ClusterPage/new.js
+++ src/pages/ClusterPage/new.js
@@ -55,9 +55,9 @@ const openSearchVersions = ['2.2.0'];
 
 let interval;
 
-export const V7_ARC = '8.6.1-cluster';
-export const V6_ARC = '8.6.1-cluster';
-export const ARC_BYOC = '8.6.1-byoc';
+export const V7_ARC = '8.4.0-cluster';
+export const V6_ARC = '8.4.0-cluster';
+export const ARC_BYOC = '8.4.0-byoc';
 export const V5_ARC = 'v5-0.0.1';
 
 export const arcVersions = {
