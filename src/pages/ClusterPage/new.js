806014f82c50af5df8afad4459c0523c535e006a
Siddharth Kothari <sids.aquarius@gmail.com>
2023-01-12 15:43:59 +0700

    chore: update reactivesearch release version

--- src/pages/ClusterPage/new.js
+++ src/pages/ClusterPage/new.js
@@ -49,9 +49,9 @@ const openSearchVersions = ['2.3.0'];
 
 let interval;
 
-export const V7_ARC = '8.8.1-cluster';
-export const V6_ARC = '8.8.1-cluster';
-export const ARC_BYOC = '8.8.1-byoc';
+export const V7_ARC = '8.9.0-cluster';
+export const V6_ARC = '8.9.0-cluster';
+export const ARC_BYOC = '8.9.0-byoc';
 export const V5_ARC = 'v5-0.0.1';
 
 export const arcVersions = {
