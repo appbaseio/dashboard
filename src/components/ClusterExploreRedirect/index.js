d097822a73fbf160854930c2ac4a8727cca5a971
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-02-07 11:59:51 +0530

    fix: redirect url handling

--- src/components/ClusterExploreRedirect/index.js
+++ src/components/ClusterExploreRedirect/index.js
@@ -37,11 +37,9 @@ const ClusterExploreRedirect = ({
 			}
 		}
 
-		const { username = '', password = '', url: arcURL = '' } = arc || {};
-		let url = `${mainURL}/?url=${arcURL.slice(
-			0,
-			-1,
-		)}&username=${username}&password=${password}&cluster=${clusterName}${arcParams}`;
+		const { username, password, url: arcURL } = arc;
+		let url = `${mainURL}/?url=${arcURL}&username=${username}&password=${password}&cluster=${clusterName}${arcParams}`;
+
 		if (cus_id) {
 			url += `&cus_id=${cus_id}`;
 		}
