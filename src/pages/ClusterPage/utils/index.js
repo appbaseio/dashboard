58acf8563ea381208a2612aacb94568078b46b8b
Siddharth Kothari <sids.aquarius@gmail.com>
2023-05-11 14:51:22 +0700

    build(search engine): update search engine versions to latest
    For ES and OS images

--- src/pages/ClusterPage/utils/index.js
+++ src/pages/ClusterPage/utils/index.js
@@ -769,9 +769,9 @@ export function updateBackend(id, body) {
 export const SSH_KEY =
 	'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCVqOPpNuX53J+uIpP0KssFRZToMV2Zy/peG3wYHvWZkDvlxLFqGTikH8MQagt01Slmn+mNfHpg6dm5NiKfmMObm5LbcJ62Nk9AtHF3BPP42WyQ3QiGZCjJOX0fVsyv3w3eB+Eq+F+9aH/uajdI+wWRviYB+ljhprZbNZyockc6V33WLeY+EeRQW0Cp9xHGQUKwJa7Ch8/lRkNi9QE6n5W/T6nRuOvu2+ThhjiDFdu2suq3V4GMlEBBS6zByT9Ct5ryJgkVJh6d/pbocVWw99mYyVm9MNp2RD9w8R2qytRO8cWvTO/KvsAZPXj6nJtB9LaUtHDzxe9o4AVXxzeuMTzx siddharth@appbase.io';
 
-export const esVersions = ['8.6.2', '7.17.8'];
+export const esVersions = ['8.7.1', '7.17.9'];
 
-export const openSearchVersions = ['2.6.0'];
+export const openSearchVersions = ['2.7.0'];
 export const ansibleMachineMarks = {
 	[CLUSTER_PLANS.SANDBOX_2020]: {
 		label: PLAN_LABEL[CLUSTER_PLANS.SANDBOX_2020],
