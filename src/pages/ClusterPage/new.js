c2352220d46d9413a41bf1310cd797953aa3aed4
Deepjyoti Barman <deep.barman30@gmail.com>
2023-05-11 12:55:58 +0530

    Bump ES version to 8.7.1 since packer image is done

--- src/pages/ClusterPage/new.js
+++ src/pages/ClusterPage/new.js
@@ -43,7 +43,7 @@ const { TabPane } = Tabs;
 const SSH_KEY =
 	'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCVqOPpNuX53J+uIpP0KssFRZToMV2Zy/peG3wYHvWZkDvlxLFqGTikH8MQagt01Slmn+mNfHpg6dm5NiKfmMObm5LbcJ62Nk9AtHF3BPP42WyQ3QiGZCjJOX0fVsyv3w3eB+Eq+F+9aH/uajdI+wWRviYB+ljhprZbNZyockc6V33WLeY+EeRQW0Cp9xHGQUKwJa7Ch8/lRkNi9QE6n5W/T6nRuOvu2+ThhjiDFdu2suq3V4GMlEBBS6zByT9Ct5ryJgkVJh6d/pbocVWw99mYyVm9MNp2RD9w8R2qytRO8cWvTO/KvsAZPXj6nJtB9LaUtHDzxe9o4AVXxzeuMTzx siddharth@appbase.io';
 
-const esVersions = ['8.7.0', '7.17.9'];
+const esVersions = ['8.7.1', '7.17.9'];
 
 const openSearchVersions = ['2.7.0'];
 
